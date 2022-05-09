40a3c7b7efb27501de7d065ad949e99baa1a0608
nvhuet <nvhuet@student.apc.edu.ph>
2022-05-09 23:06:57 +0800

    Menus Fixed and Assets Added

--- ELECTV2 Midterm Project/Assets/Test/UI/PauseScreen.cs
+++ ELECTV2 Midterm Project/Assets/Test/UI/PauseScreen.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class PauseScreen : MonoBehaviour
 {
-    public GameObject pauseUI, MainUI;
+    public GameObject pauseUI, MainUI, HowToPlayUI, pauseDefault;
     private void Start()
     {
         Time.timeScale = 1;
@@ -23,8 +23,19 @@ public class PauseScreen : MonoBehaviour
         pauseUI.SetActive(false);
         MainUI.SetActive(true);
     }
+    public void HowtoPlayButton()
+    {
+        pauseDefault.SetActive(false);
+        HowToPlayUI.SetActive(true);
+    }
+    public void BackButton()
+    {
+        pauseDefault.SetActive(true);
+        HowToPlayUI.SetActive(false);
+    }
     public void QuitButton()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        Time.timeScale = 1;
     }
 }
